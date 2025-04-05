da6987ef0ec1c4c38181ac45c1f19174907f0213
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2025-04-04 23:55:23 -0700

    todo: look up optinal params and how that logic works in Go

--- api/data_interface/reader.go
+++ api/data_interface/reader.go
@@ -2,12 +2,29 @@ package data_interface
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
+var file_name = "data/food_db.csv";
+
+type FoodData struct {
+	name string
+	enabled bool
+	last_date_tried time.Time
+	likes bool
+}
+
+func newFoodData(name string, enabled bool, last_date_tried time.Time, likes bool) *FoodData {
+
+	
+}
+
 func Print_csv() {
-	file, err := os.Open("data/food_db.csv")
+	file, err := os.Open(file_name)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
@@ -24,4 +41,30 @@ func Print_csv() {
 	for _, record := range records {
 		fmt.Println(record)
 	}
+}
+
+// chage this to write_rows when struct is created, like just take in []string
+func Write_row(food_name string, enabled bool, last_date_tried time.Time, likes bool) bool {
+	file, err := os.Open(file_name)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return false
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading CSV:", err)
+		return false
+	}
+
+	for i, record := range records {
+		if record[0] == food_name {
+			records[i] = []string{food_name, strconv.FormatBool(enabled), last_date_tried.String(), strconv.FormatBool(likes)}
+			return true
+		}
+	}
+
+	return false
 }
