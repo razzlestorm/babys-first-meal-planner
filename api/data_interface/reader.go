b48f10e8915335280895787c5f28774aa048c774
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2025-04-05 00:29:23 -0700

    add writing to CSV, TODO: figure out file permissions or just move to DB

--- api/data_interface/reader.go
+++ api/data_interface/reader.go
@@ -2,7 +2,6 @@ package data_interface
 
 import (
 	"encoding/csv"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -20,13 +19,25 @@ type FoodData struct {
 
 func newFoodData(name string, enabled bool, last_date_tried time.Time, likes bool) *FoodData {
 
-	
+	fd := FoodData{name, enabled, last_date_tried, likes}	
+	return &fd
 }
 
+func (fd *FoodData) ToStringSlice() []string {
+	return []string{fd.name, strconv.FormatBool(fd.enabled), fd.last_date_tried.String(), strconv.FormatBool(fd.likes)}
+
+} 
+
 func Print_csv() {
-	file, err := os.Open(file_name)
+	_, serr := os.Stat(file_name)
+	
+	if serr != nil {
+		fmt.Printf("Error stat finding file: %v", serr)
+		return
+	}
+	file, err := os.OpenFile(file_name, os.O_RDWR, 0644)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		fmt.Printf("Error opening file: %v", err)
 		return
 	}
 	defer file.Close()
@@ -45,9 +56,10 @@ func Print_csv() {
 
 // chage this to write_rows when struct is created, like just take in []string
 func Write_row(food_name string, enabled bool, last_date_tried time.Time, likes bool) bool {
+	fd := newFoodData(food_name, enabled, last_date_tried, likes)
 	file, err := os.Open(file_name)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		fmt.Println("Error opening file: %v", err)
 		return false
 	}
 	defer file.Close()
@@ -59,11 +71,26 @@ func Write_row(food_name string, enabled bool, last_date_tried time.Time, likes 
 		return false
 	}
 
+	written := false
+
 	for i, record := range records {
 		if record[0] == food_name {
-			records[i] = []string{food_name, strconv.FormatBool(enabled), last_date_tried.String(), strconv.FormatBool(likes)}
-			return true
+			records[i] = fd.ToStringSlice()
+			written = true
 		}
+	}
+
+	if written == true {
+		file.Seek(0, 0)
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+		err = writer.WriteAll(records)
+
+		if err != nil {
+			fmt.Println("Error writing to CSV:", err)
+			return false
+		}
+		return true
 	}
 
 	return false
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"time"
 	"github.com/razzlestorm/babys-first-meal-planner/api/data_interface"
 )
 func main() {
 	
-	data_interface.Print_csv()
+	// data_interface.Print_csv()
+	test := data_interface.Write_row("Egg", true, time.Now(), true)
+	println(test)
 }
