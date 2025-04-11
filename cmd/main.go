3c2cfd43347b055c836256a02553f5ba6858bf79
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2025-04-10 23:17:01 -0700

    add surrealdb boilerplate to play around with

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,79 @@
 package main
 
 import (
+	"fmt"
+	surrealdb "github.com/surrealdb/surrealdb.go"
+	"github.com/surrealdb/surrealdb.go/pkg/models"
 	"time"
-	"github.com/razzlestorm/babys-first-meal-planner/api/data_interface"
+	// "github.com/razzlestorm/babys-first-meal-planner/api/data_interface"
 )
+
+type FoodData struct {
+	ID      	*models.RecordID `json:"id,omitempty"`
+	Name    	string `json:"name"`
+	Enabled 	bool
+	LastDateTried 	time.Time
+	Likes		bool
+}
+
 func main() {
 	
+		// Connect to SurrealDB
+	db, err := surrealdb.New("ws://localhost:8000")
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the namespace and database
+	if err = db.Use("testNS", "testDB"); err != nil {
+		panic(err)
+	}
+
+	// Sign in to authentication `db`
+	authData := &surrealdb.Auth{
+		Username: "root", // use your setup username
+		Password: "root", // use your setup password
+	}
+	token, err := db.SignIn(authData)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check token validity. This is not necessary if you called `SignIn` before. This authenticates the `db` instance too if sign in was
+	// not previously called
+	if err := db.Authenticate(token); err != nil {
+		panic(err)
+	}
+
+	// And we can later on invalidate the token if desired
+	defer func(token string) {
+		if err := db.Invalidate(); err != nil {
+			panic(err)
+		}
+	}(token)
 	// data_interface.Print_csv()
-	test := data_interface.Write_row("Egg", true, time.Now(), true)
-	println(test)
+	// test := data_interface.Write_row("Egg", true, time.Now(), true)
+	// println(test)// Or use structs
+	food1, err := surrealdb.Create[FoodData](db, models.Table("food_data"), FoodData{
+		Name:     "Egg",
+		Enabled:  true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created food data with a struct: %+v\n", food1)
+
+	// Get entry by Record ID
+	food, err := surrealdb.Select[FoodData, models.RecordID](db, *food1.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Selected a food by record id: %+v\n", food)
+
+	// Or retrieve the entire table
+	foods, err := surrealdb.Select[[]FoodData, models.Table](db, models.Table("food_data"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Selected all in food_data table: %+v\n", foods)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/razzlestorm/babys-first-meal-planner
 
 go 1.22.2
+
+require github.com/surrealdb/surrealdb.go v0.3.2
+
+require (
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
